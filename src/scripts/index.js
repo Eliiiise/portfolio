94df1b97a7ac17cba7360f3a6ca04db84d61256c
Eliiiise <balvalloire@gmail.com>
2020-02-01 16:16:07 +0100

    transition

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,13 +8,13 @@ import { closeProject } from "./components/closeProject";
 import { projectParallax } from "./components/projectParallax";
 import { nextProject } from "./components/nextProject";
 
-//closeProject();
-//projectParallax();
-//nextProject();
+closeProject();
+projectParallax();
+nextProject();
 
 import { textEffect } from "./components/textEffect";
 import { chooseProject } from "./components/chooseProject";
 
-textEffect();
-chooseProject();
+//textEffect();
+//chooseProject();
 
